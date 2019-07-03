de1cd2dea066467c9cf52ecd9f3cccf602a4efc5
Casey J. Morris <caseyjmorris@gmail.com>
2019-07-02 23:04:00 -0400

    playlist mgr

---
+++ PSCBuddy.Behaviors/Utils/PlaylistManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PSCBuddy.Behaviors.JsonModels;
+
+namespace PSCBuddy.Behaviors.Utils
+{
+  public class PlaylistManager
+  {
+    public void UpdatePlaylist(string driveRoot, string playlistName, IEnumerable<string> gamePaths,
+      string corePath = "DETECT", string coreName = "DETECT")
+    {
+      if (!driveRoot.EndsWith("\\"))
+      {
+        driveRoot += "\\";
+      }
+      var playlistDirectory = Path.Combine(driveRoot, @"bleemsync\opt\retroarch\.config\retroarch\playlists");
+      if (!Directory.Exists(playlistDirectory))
+      {
+        throw new ArgumentException("Drive does not exist or is not RetroArch drive", nameof(driveRoot));
+      }
+
+      var playlistPath = Path.Combine(playlistDirectory, playlistName + ".lpl");
+      PlaylistJson json;
+
+      if (File.Exists(playlistPath))
+      {
+        json = JsonConvert.DeserializeObject<PlaylistJson>(File.ReadAllText(playlistPath));
+      }
+      else
+      {
+        json = new PlaylistJson
+        {
+          version = "1.0",
+          items = new List<PlaylistItemJson>(),
+        };
+      }
+
+      foreach (var gamePath in gamePaths)
+      {
+        var item = new PlaylistItemJson
+        {
+          core_name = coreName,
+          core_path = corePath,
+          db_name = $"{playlistName}.lpl",
+          label = Path.GetFileNameWithoutExtension(gamePath),
+          path = this.ConvertPath(gamePath),
+          crc32 = "DETECT",
+        };
+
+        json.items.Add(item);
+      }
+
+      json.items = json.items.OrderBy(i => i.label).ToList();
+
+      File.WriteAllText(playlistPath, JsonConvert.SerializeObject(json, Formatting.Indented));
+    }
+
+    private string ConvertPath(string originalPath)
+    {
+      var sb = new StringBuilder("/media");
+      var i = 0;
+      foreach (var character in originalPath)
+      {
+        if (i++ < 2)
+        {
+          continue;
+        }
+
+        sb.Append(character == '\\' ? '/' : character);
+      }
+
+      return sb.ToString();
+    }
+  }
+}
--- PSCBuddy.UI/Program.cs
+++ PSCBuddy.UI/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PSCBuddy.Behaviors.Utils;
 
 namespace PSCBuddy.UI
 {
-  static class Program
+  internal static class Program
   {
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main ()
+    private static void Main()
     {
+      //var pm = new PlaylistManager();
+      //pm.UpdatePlaylist("L:", "PSX CHD", Directory.GetFiles(@"L:\roms\PSX CHD"),
+      //  "/media/bleemsync/opt/retroarch/.config/retroarch/cores/pcsx_rearmed_libretro.so", "PCSX ReARMed");
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new ArchiveCHDConversionWindow());
